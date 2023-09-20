6e090bb5009e6886e5e09afbe66be802065d045a
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-19 20:29:54 -0400

    phase-1-lecture-videos-dom-manipulation-review/
    phase-1-lecture-videos-dom-manipulation-review/

---
+++ phase-1-lecture-videos-dom-manipulation-review-main/data.js
@@ -0,0 +1,26 @@
+onst animalData = [
+    {
+        name: "Red panda",
+        imageUrl: "http://il.up.com/www.rdpandanetwork.org/",
+        description: "not actually a panda",
+        donations: 0,
+    },
+    {
+        name: "Pangolin",
+        imageUrl: "http://pinning.com/originals/bf/ff/93/bf",
+        description: "The Pangolin, otherwise known as the scaly",
+        donations: 0,
+    },
+    {
+        name: "Mantis Shrimp",
+        imageUrl: "http://media.wored.com/photos/5bc705bf86",
+        description: "Gibbons communicate by singing! Gibbon vo",
+        donations: 0,
+    },
+    {
+        name: "Wonbat",
+        imageUrl: "https://external-preview.read.it/hgv?6hMT",
+        description: "They have code-shaped poop. Wonbat poop is",
+        donations: 0,
+    },
+];
